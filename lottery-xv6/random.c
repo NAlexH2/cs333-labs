3b9c43a45778f6aa5aae98c503ec92a47d0fe56b
NAlexH2 <nharris356087@gmail.com>
2022-10-21 14:59:38 -0700

    fixed something silly oops

--- lottery-xv6/random.c
+++ lottery-xv6/random.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
 #ifdef PROC_TIMES
     if(argc > 1)
-        srand((int)argv[1]);
+        srand(atoi(argv[1]));
         
     for(int i = 0; i < 10; ++i)
         printf(1, "random number is: %u\n", rand());
