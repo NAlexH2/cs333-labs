83dfd417c14d43dc61cc8cf099d95b828a01a927
NAlexH2 <nharris356087@gmail.com>
2022-11-03 17:24:30 -0700

    just some headers

---
+++ 3-OptimusPrime/primesMT.h
@@ -0,0 +1,13 @@
+// Alex Harris - CS 333 Winter 2022
+
+# include <stdlib.h>
+# include <stdint.h>
+# include <limits.h>
+# include <stdio.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <math.h>
+# include <string.h>
+# include <getopt.h>
+# include <pthread.h>
