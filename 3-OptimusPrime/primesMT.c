dff956b6fa0fa32d9587db485d09b8922beda5d1
NAlexH2 <nharris356087@gmail.com>
2022-11-07 18:23:34 -0800

    Just messing around with bits

--- 3-OptimusPrime/primesMT.c
+++ 3-OptimusPrime/primesMT.c
@@ -1,16 +1,17 @@
 // Alex Harris - CS 333 Winter 2022
 #include "primesMT.h"
 
-static int isVerbose = 0;
+static int is_verbose = 0;
 static unsigned int user_bounds = 10240;
 static unsigned int user_threads = 1;
+static unsigned int bit_arr_size = 0;
 
 // User defined bounds. Defaults to 10240 if user doesn't enter a number.
 #define UBOUNDS user_bounds
 // User defined # of threads. Defaults to 1 if not specified by the user.
 #define UTHREADS user_threads
-#define BITARRAYSIZE 0
-#define VERBOSE isVerbose
+#define ARRAYSIZE bit_arr_size
+#define VERBOSE is_verbose
 #define FILE_NAME "test.out"
 
 void helpMe(void);
@@ -21,7 +22,6 @@ int main(int argc, char *argv[])
     // long tid = 0;
     FILE *op = NULL;
     BitBlock_t *BitArray;
-    int BitArraySize = 0;
     {
         int opt = -1;
         while ((opt = getopt(argc, argv, "t:u:vh")) != -1)
@@ -30,7 +30,6 @@ int main(int argc, char *argv[])
             {
             case 't':
                 UTHREADS = atoi(optarg);
-                BITARRAYSIZE = (UBOUNDS / 32) + 1;
                 break;
             // bounds
             case 'u':
@@ -48,19 +47,30 @@ int main(int argc, char *argv[])
             }
         }
     }
-    BitArray = malloc((BITARRAYSIZE * sizeof(BitBlock_t));
-    for(int i = 0; i <)
+    ARRAYSIZE = (UBOUNDS / 32) + 1;
+    BitArray = malloc(ARRAYSIZE * sizeof(BitBlock_t));
 
-    printf("\nhello world!\n");
     op = fopen(FILE_NAME, "w");
     fprintf(op, "test");
     fclose(op);
-    perror("\n\nThis is stderror\n\n");
     printf("\nuser_bounds = %u\nuser_threads = %u\nverbose = %d\n",
            UBOUNDS, UTHREADS, VERBOSE);
 
     printf("\nBitArray slots = %d\t BitArray[0] Size = %lu\n",
-          BITARRAYSIZE, sizeof(*BitArray));
+          ARRAYSIZE, sizeof(*BitArray));
+
+    for(int i = 0; i < ARRAYSIZE; ++i){
+        BitArray[i].bits = UINT32_MAX;
+        printf("\n%u\n",BitArray[i].bits);
+    }
+    // uint32_t i = 1;
+    // while(i != 0){
+    //     if(BitArray[0].bits & i)
+    //         printf("\nSuccess!, i = %u\n", i);
+    //     else
+    //         printf("\nWOMP WOMP :( \n");
+    //     i = i << 1;
+    // }
     exit(EXIT_SUCCESS);
 }
 
